04f18dcb202bdb6c1cae414aa5f9ea22e7a44874
franciscodevitt <fdevitt@fi.uba.ar>
2024-07-18 21:56:47 -0300

    Le agregue las Foreign keys a las tablas y el relationship

--- Back/tablas.py
+++ Back/tablas.py
@@ -3,30 +3,31 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Usuarios(db.Model):
+class Usuario(db.Model):
     __tablename__ = 'ususarios'
-    id_usuario = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True)
     nombre = db.Column(db.String(50), unique = True)
     contraseña = db.Column(db.String(50), nullable = False)
-    puntos = db.Column(db.Integer)         #cambiar a flotante
+    puntos = db.Column(db.Float)        
     email = db.Column(db.String(50))
+    estados = db.relationship("EstadoSalud")
 
-class Grupos(db.Model):
+class Grupo(db.Model):
     __tablename__ = 'grupos'
     id_grupo = db.Column(db.Integer, primary_key = True)
     nombre_grupo = db.Column(db.String(50), unique = True)
     contraseña = db.Column(db.String(50), nullable = False)
     cant_integrantes = db.Column(db.Integer)
 
-class SaludUsuarios(db.Model):
-    __tablename__ = 'salud_usuarios'
-    id_usuario = db.Column(db.Integer, primary_key = True) # relacionar con usuarios
+class EstadoSalud(db.Model):
+    __tablename__ = 'estados_salud'
+    id_usuario = db.Column(db.Integer, db.ForeignKey('usuarios.id'), primary_key = True)
     dia = db.Column(db.String(15), primary_key = True)
-    agua = db.Column(db.Integer)
-    sueño = db.Column(db.Integer)          # En sueño y Entrenamiento
-    entrenamiento =db.Column(db.Integer)   # cambiar el tipo a flotante
+    agua = db.Column(db.Float)
+    sueño = db.Column(db.Float)          
+    entrenamiento =db.Column(db.Float)  
 
-class Asignaciones(db.Model):
+class Asignacion(db.Model):
     __tablename__ = 'asignaciones'
-    id_usuario = db.Column(db.Integer, primary_key = True) #Relacionarla con usuarios y con grupo
-    id_grupo = db.Column(db.Integer, primary_key = True)
+    id_usuario = db.Column(db.Integer, db.ForeignKey('usuarios.id'), primary_key = True) #Relacionarla con usuarios y con grupo
+    id_grupo = db.Column(db.Integer, db.ForeignKey('grupos.id'), primary_key = True)
