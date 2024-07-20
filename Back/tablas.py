d0353a5ebcaa541e8727368a0a5eb4a9cc302418
franciscodevitt <fdevitt@fi.uba.ar>
2024-07-20 16:23:19 -0300

    Se modifico las columnas Id en los modelos de la tabla usuarios y de la tabla grupo para que se incrementen automaticamente

--- Back/tablas.py
+++ Back/tablas.py
@@ -4,19 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Usuario(db.Model):
-    __tablename__ = 'ususarios'
-    id = db.Column(db.Integer, primary_key = True)
+    __tablename__ = 'usuarios'
+    id = db.Column(db.Integer,db.Sequence("some_id_seq", start=1), primary_key = True)
     nombre = db.Column(db.String(50), unique = True)
-    contraseña = db.Column(db.String(50), nullable = False)
+    password = db.Column(db.String(50), nullable = False)
     puntos = db.Column(db.Float)        
-    email = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique = True)
     estados = db.relationship("EstadoSalud")
 
 class Grupo(db.Model):
     __tablename__ = 'grupos'
-    id_grupo = db.Column(db.Integer, primary_key = True)
+    id_grupo = db.Column(db.Integer,db.Sequence("some_id_seq", start=1), primary_key = True)
     nombre_grupo = db.Column(db.String(50), unique = True)
-    contraseña = db.Column(db.String(50), nullable = False)
+    password = db.Column(db.String(50), nullable = False)
     cant_integrantes = db.Column(db.Integer)
 
 class EstadoSalud(db.Model):
@@ -30,4 +30,4 @@ class EstadoSalud(db.Model):
 class Asignacion(db.Model):
     __tablename__ = 'asignaciones'
     id_usuario = db.Column(db.Integer, db.ForeignKey('usuarios.id'), primary_key = True) #Relacionarla con usuarios y con grupo
-    id_grupo = db.Column(db.Integer, db.ForeignKey('grupos.id'), primary_key = True)
+    id_grupo = db.Column(db.Integer, db.ForeignKey('grupos.id_grupo'), primary_key = True)
